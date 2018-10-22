cae27d321c29834afba37be2cfbf9aef21e4ee7a
thiren <thiren@filepounder.com>
2018-10-22 13:38:21 +0200

    beautified code

--- test/1. MonogQuery.js
+++ test/1. MonogQuery.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
-const mongo=require('mongodb');
-const async=require('async');
-const MongoQuery=require('../lib').MongoQuery;
+const mongo = require('mongodb');
+const async = require('async');
+const MongoQuery = require('../lib').MongoQuery;
 
-var dbConfig= {
+var dbConfig = {
     "connectionString": "mongodb://127.0.0.1/mongoutilstests",
     "connectionOptions": {
         "server": {
@@ -15,18 +15,18 @@ var dbConfig= {
     }
 }
 
-var _db=null;
+var _db = null;
 
-describe('Mongo Query', function() {
+describe('Mongo Query', function () {
     before(function (done) {
 
 
         function dbConnected(err, database) {
-            if (err)return done(err);
-            _db=database;
+            if (err) return done(err);
+            _db = database;
             // runs before all tests in this block
             _db.collections(function (err, collections) {
-                if (err)return done(err);
+                if (err) return done(err);
                 async.each(collections,
                     function (item, itemCallback) {
                         item.deleteMany({}, itemCallback);
@@ -46,68 +46,75 @@ describe('Mongo Query', function() {
     });
 
 
-    it('should throw an error when no config specified', function() {
-       assert.throws(function(){
-           let mongoQuery=new MongoQuery();
+    it('should throw an error when no config specified', function () {
+        assert.throws(function () {
+            let mongoQuery = new MongoQuery();
 
-       },Error,"No error thrown")
+        }, Error, "No error thrown")
     });
 
-    it('should parse a simple query string', function() {
-        let mongoQuery=new MongoQuery("$sort=-field1,field2&$select=field1,field2");
+    it('should parse a simple query string', function () {
+        let mongoQuery = new MongoQuery("$sort=-field1,field2&$select=field1,field2");
 
-        assert.deepEqual(mongoQuery.parsedQuery.select,{field1:true,field2:true},"Invalid Select")
-        assert.deepEqual(mongoQuery.parsedQuery.sort,{field1:-1,field2:1},"Invalid Sort")
+        assert.deepEqual(mongoQuery.parsedQuery.select, {field1: true, field2: true}, "Invalid Select")
+        assert.deepEqual(mongoQuery.parsedQuery.sort, {field1: -1, field2: 1}, "Invalid Sort")
     });
 
-    it('should parse a complex select with inclusions', function() {
-        let mongoQuery=new MongoQuery("$select=field1,field2");
+    it('should parse a complex select with inclusions', function () {
+        let mongoQuery = new MongoQuery("$select=field1,field2");
 
-        assert.deepEqual(mongoQuery.parsedQuery.select,{field1:true,field2:true},"Invalid Select")
+        assert.deepEqual(mongoQuery.parsedQuery.select, {field1: true, field2: true}, "Invalid Select")
     });
 
-    it('should parse a complex select with exclusions', function() {
-        let mongoQuery=new MongoQuery("$select=-field1,-field2");
+    it('should parse a complex select with exclusions', function () {
+        let mongoQuery = new MongoQuery("$select=-field1,-field2");
 
-        assert.deepEqual(mongoQuery.parsedQuery.select,{field1:false,field2:false},"Invalid Select")
+        assert.deepEqual(mongoQuery.parsedQuery.select, {field1: false, field2: false}, "Invalid Select")
     });
 
-    it('should throw an error on a select with exclusions and inclusions', function() {
-        assert.throws(function(){
-            let mongoQuery=new MongoQuery("$select=field1,-field2");
+    it('should throw an error on a select with exclusions and inclusions', function () {
+        assert.throws(function () {
+            let mongoQuery = new MongoQuery("$select=field1,-field2");
 
-        },Error,"No error thrown")
+        }, Error, "No error thrown")
     });
 
-    it('should throw an error on a select with exclusions and inclusions 2', function() {
-        assert.throws(function(){
-            let mongoQuery=new MongoQuery("$select=-field1,field2");
+    it('should throw an error on a select with exclusions and inclusions 2', function () {
+        assert.throws(function () {
+            let mongoQuery = new MongoQuery("$select=-field1,field2");
 
-        },Error,"No error thrown")
+        }, Error, "No error thrown")
     });
 
 
-    it('should parse a complex filter', function() {
-        let mongoQuery=new MongoQuery("$filter=field1/field2 eq 'a'");
+    it('should parse a complex filter', function () {
+        let mongoQuery = new MongoQuery("$filter=field1/field2 eq 'a'");
 
-        assert.deepEqual(mongoQuery.parsedQuery.query,{"field1.field2":"a"},"Invalid Select")
+        assert.deepEqual(mongoQuery.parsedQuery.query, {"field1.field2": "a"}, "Invalid Select")
     });
 
-    it('should parse a raw query', function() {
-        let mongoQuery=new MongoQuery('$rawQuery={"field1.field2":{"$date":"2016-01-01T00:00:00Z"}}');
+    it('should parse a raw query', function () {
+        let mongoQuery = new MongoQuery('$rawQuery={"field1.field2":{"$date":"2016-01-01T00:00:00Z"}}');
 
-        assert.deepEqual(mongoQuery.parsedQuery.query,{"field1.field2":new Date("2016-01-01T00:00:00Z")},"Invalid Raw Query")
+        assert.deepEqual(mongoQuery.parsedQuery.query, {"field1.field2": new Date("2016-01-01T00:00:00Z")}, "Invalid Raw Query")
     });
 
-    it('should parse a raw query and filter', function() {
-        let mongoQuery=new MongoQuery('$filter=field2 eq \'a\'&$rawQuery={"field1":{"$date":"2016-01-01T00:00:00Z"}}');
+    it('should parse a raw query and filter', function () {
+        let mongoQuery = new MongoQuery('$filter=field2 eq \'a\'&$rawQuery={"field1":{"$date":"2016-01-01T00:00:00Z"}}');
 
-        assert.deepEqual(mongoQuery.parsedQuery.query,{field2:"a","field1":new Date("2016-01-01T00:00:00Z")},"Invalid  Query")
+        assert.deepEqual(mongoQuery.parsedQuery.query, {
+            field2: "a",
+            "field1": new Date("2016-01-01T00:00:00Z")
+        }, "Invalid  Query")
     });
 
-    it('should parse a raw query and filter with a default', function() {
-        let mongoQuery=new MongoQuery('$filter=field2 eq \'a\'&$rawQuery={"field1":{"$date":"2016-01-01T00:00:00Z"}}',{field3:3});
+    it('should parse a raw query and filter with a default', function () {
+        let mongoQuery = new MongoQuery('$filter=field2 eq \'a\'&$rawQuery={"field1":{"$date":"2016-01-01T00:00:00Z"}}', {field3: 3});
 
-        assert.deepEqual(mongoQuery.parsedQuery.query,{field2:"a","field1":new Date("2016-01-01T00:00:00Z"),field3:3},"Invalid  Query")
+        assert.deepEqual(mongoQuery.parsedQuery.query, {
+            field2: "a",
+            "field1": new Date("2016-01-01T00:00:00Z"),
+            field3: 3
+        }, "Invalid  Query")
     });
 });
--- test/2. Collection.js
+++ test/2. Collection.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
-const mongo=require('mongodb');
-const async=require('async');
-const Collection=require('../lib').Collection;
+const mongo = require('mongodb');
+const async = require('async');
+const Collection = require('../lib').Collection;
 const _s = require('underscore.string');
-const moment=require('moment');
-const $stream=require('stream');
-const MongoQuery=require('../lib').MongoQuery;
+const moment = require('moment');
+const $stream = require('stream');
+const MongoQuery = require('../lib').MongoQuery;
 
-var dbConfig= {
+var dbConfig = {
     "connectionString": "mongodb://127.0.0.1/mongoutilstests",
     "connectionOptions": {
         "server": {
@@ -19,18 +19,18 @@ var dbConfig= {
     }
 }
 
-var _db=null;
+var _db = null;
 
-describe('Collection', function() {
+describe('Collection', function () {
     before(function (done) {
 
 
         function dbConnected(err, database) {
-            if (err)return done(err);
-            _db=database;
+            if (err) return done(err);
+            _db = database;
             // runs before all tests in this block
             _db.collections(function (err, collections) {
-                if (err)return done(err);
+                if (err) return done(err);
                 async.each(collections,
                     function (item, itemCallback) {
                         item.deleteMany({}, itemCallback);
@@ -50,42 +50,42 @@ describe('Collection', function() {
     });
 
 
-    it('should throw an error when no config specified', function() {
-       assert.throws(function(){
-           let collection=new Collection();
+    it('should throw an error when no config specified', function () {
+        assert.throws(function () {
+            let collection = new Collection();
 
-       },Error,"No error thrown")
+        }, Error, "No error thrown")
     });
 
-    describe('query',function(){
+    describe('query', function () {
         before(function (done) {
             _db.collection("testquery").insertMany([
                 {
-                    val:"a"
+                    val: "a"
                 },
                 {
-                    val:"b"
+                    val: "b"
                 }
-            ],function(err){
+            ], function (err) {
                 return done(err)
             });
         });
 
-        it('should stream results', function(done) {
-            let collection=new Collection(_db.collection("testquery"));
-            let ws=new $stream.Writable({objectMode:true})
-            let writeCnt=0;
-            ws._write=function(chunk,encoding,done){
+        it('should stream results', function (done) {
+            let collection = new Collection(_db.collection("testquery"));
+            let ws = new $stream.Writable({objectMode: true})
+            let writeCnt = 0;
+            ws._write = function (chunk, encoding, done) {
                 writeCnt++;
                 return done();
             }
 
-            ws.on('finish',function(){
-                assert.equal(writeCnt,2,"Invalid writes")
+            ws.on('finish', function () {
+                assert.equal(writeCnt, 2, "Invalid writes")
                 return done();
             })
 
-            let mQuery=new MongoQuery({limit:1000});
+            let mQuery = new MongoQuery({limit: 1000});
             collection.queryAsStream(mQuery).pipe(ws)
 
 
@@ -94,94 +94,96 @@ describe('Collection', function() {
 
     });
 
-    describe('Stats', function() {
+    describe('Stats', function () {
         before(function (done) {
             _db.collection("teststats").insertMany([
                 {
-                    val:"a"
+                    val: "a"
                 },
                 {
-                    val:"b"
+                    val: "b"
                 }
-            ],function(err){
+            ], function (err) {
                 return done(err)
             });
         });
 
-        it('should throw an error on invalid config 1', function() {
-            assert.throws(function(){
-                let collection=new Collection(_db.collection("teststats"));
-                collection.updateStats({},function(){})
+        it('should throw an error on invalid config 1', function () {
+            assert.throws(function () {
+                let collection = new Collection(_db.collection("teststats"));
+                collection.updateStats({}, function () {
+                })
 
-            },Error,"No error thrown")
+            }, Error, "No error thrown")
         });
 
-        it('should throw an error on invalid config 2', function() {
-            assert.throws(function(){
-                let collection=new Collection(_db.collection("teststats"));
-                collection.updateStats({statsField:"123"},function(){})
+        it('should throw an error on invalid config 2', function () {
+            assert.throws(function () {
+                let collection = new Collection(_db.collection("teststats"));
+                collection.updateStats({statsField: "123"}, function () {
+                })
 
-            },Error,"No error thrown")
+            }, Error, "No error thrown")
         });
 
-        it('should update stats', function(done) {
-            let date=new Date();
-            let momentDate=new moment(date).utc();
-            let year= momentDate.year();
-            let month=_s.lpad(momentDate.month()+1,2,'0');
-            let day=_s.lpad(momentDate.date(),2,'0');
-            let hour= _s.lpad(momentDate.hour(),2,'0');
+        it('should update stats', function (done) {
+            let date = new Date();
+            let momentDate = new moment(date).utc();
+            let year = momentDate.year();
+            let month = _s.lpad(momentDate.month() + 1, 2, '0');
+            let day = _s.lpad(momentDate.date(), 2, '0');
+            let hour = _s.lpad(momentDate.hour(), 2, '0');
 
-            let collection=new Collection(_db.collection("teststats"));
+            let collection = new Collection(_db.collection("teststats"));
             collection.updateStats({
-                statsField:"stats1",
-                date:new Date(),
-                query:{val:"a"},
-                increments:{
-                    field:"counter",
-                    value:1
+                statsField: "stats1",
+                date: new Date(),
+                query: {val: "a"},
+                increments: {
+                    field: "counter",
+                    value: 1
                 }
-            },function(err){
-                assert(!err,"Error Occurred");
-                _db.collection("teststats").findOne({val:"a"},function(err,result){
-                    assert(!err,"Error Occurred");
-                    assert.equal(result.stats1.counter,1,"Invalid stats value")
-                    assert.equal(result.stats1[year].counter,1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month].counter,1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month][day].counter,1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month][day][hour].counter,1,"Invalid stats value")
+            }, function (err) {
+                assert(!err, "Error Occurred");
+                _db.collection("teststats").findOne({val: "a"}, function (err, result) {
+                    assert(!err, "Error Occurred");
+                    assert.equal(result.stats1.counter, 1, "Invalid stats value")
+                    assert.equal(result.stats1[year].counter, 1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month].counter, 1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month][day].counter, 1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month][day][hour].counter, 1, "Invalid stats value")
                     return done();
                 })
             })
 
         });
 
-        it('should update stats at a different date', function(done) {
-            let date=new Date();
-            let momentDate=new moment(date).utc().subtract(1,'month');
-            let year= momentDate.year();
-            let month=_s.lpad(momentDate.month()+1,2,'0');
-            let day=_s.lpad(momentDate.date(),2,'0');
-            let hour= _s.lpad(momentDate.hour(),2,'0');
+        it('should update stats at a different date', function (done) {
+            let date = new Date();
+            let momentDate = new moment(date).utc().subtract(1, 'month');
+            let year = momentDate.year();
+            let month = _s.lpad(momentDate.month() + 1, 2, '0');
+            let day = _s.lpad(momentDate.date(), 2, '0');
+            let hour = _s.lpad(momentDate.hour(), 2, '0');
 
-            let collection=new Collection(_db.collection("teststats"));
+            let collection = new Collection(_db.collection("teststats"));
             collection.updateStats({
-                statsField:"stats1",
-                date:momentDate.toDate(),
-                query:{val:"a"},
-                increments:{
-                    field:"counter",
-                    value:10
+                statsField: "stats1",
+                date: momentDate.toDate(),
+                query: {val: "a"},
+                increments: {
+                    field: "counter",
+                    value: 10
                 }
-            },function(err){
-                assert(!err,"Error Occurred");
-                _db.collection("teststats").findOne({val:"a"},function(err,result){
-                    assert(!err,"Error Occurred");
-                    assert.equal(result.stats1.counter,11,"Invalid stats value")
-                    assert.equal(result.stats1[year].counter,11,"Invalid stats value")
-                    assert.equal(result.stats1[year][month].counter,10,"Invalid stats value")
-                    assert.equal(result.stats1[year][month][day].counter,10,"Invalid stats value")
-                    assert.equal(result.stats1[year][month][day][hour].counter,10,"Invalid stats value")
+            }, function (err) {
+                assert(!err, "Error Occurred");
+                _db.collection("teststats").findOne({val: "a"}, function (err, result) {
+                    assert(!err, "Error Occurred");
+                    assert.equal(result.stats1.counter, 11, "Invalid stats value")
+                    assert.equal(result.stats1[year].counter, 11, "Invalid stats value")
+                    assert.equal(result.stats1[year][month].counter, 10, "Invalid stats value")
+                    assert.equal(result.stats1[year][month][day].counter, 10, "Invalid stats value")
+                    assert.equal(result.stats1[year][month][day][hour].counter, 10, "Invalid stats value")
                     return done();
                 })
             })
@@ -189,51 +191,47 @@ describe('Collection', function() {
         });
 
 
-        it('should update mulyiple stats', function(done) {
-            let date=new Date();
-            let momentDate=new moment(date).utc();
-            let year= momentDate.year();
-            let month=_s.lpad(momentDate.month()+1,2,'0');
-            let day=_s.lpad(momentDate.date(),2,'0');
-            let hour= _s.lpad(momentDate.hour(),2,'0');
+        it('should update mulyiple stats', function (done) {
+            let date = new Date();
+            let momentDate = new moment(date).utc();
+            let year = momentDate.year();
+            let month = _s.lpad(momentDate.month() + 1, 2, '0');
+            let day = _s.lpad(momentDate.date(), 2, '0');
+            let hour = _s.lpad(momentDate.hour(), 2, '0');
 
-            let collection=new Collection(_db.collection("teststats"));
+            let collection = new Collection(_db.collection("teststats"));
             collection.updateStats({
-                statsField:"stats1",
-                date:new Date(),
-                query:{val:"b"},
-                increments:[
+                statsField: "stats1",
+                date: new Date(),
+                query: {val: "b"},
+                increments: [
                     {
-                        field:"counter1",
-                        value:-1
+                        field: "counter1",
+                        value: -1
                     },
                     {
-                        field:"counter2",
-                        value:1
+                        field: "counter2",
+                        value: 1
                     }
                 ]
-            },function(err){
-                assert(!err,"Error Occurred");
-                _db.collection("teststats").findOne({val:"b"},function(err,result){
-                    assert(!err,"Error Occurred");
-                    assert.equal(result.stats1.counter1,-1,"Invalid stats value")
-                    assert.equal(result.stats1[year].counter1,-1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month].counter1,-1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month][day].counter1,-1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month][day][hour].counter1,-1,"Invalid stats value")
-                    assert.equal(result.stats1.counter2,1,"Invalid stats value")
-                    assert.equal(result.stats1[year].counter2,1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month].counter2,1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month][day].counter2,1,"Invalid stats value")
-                    assert.equal(result.stats1[year][month][day][hour].counter2,1,"Invalid stats value")
+            }, function (err) {
+                assert(!err, "Error Occurred");
+                _db.collection("teststats").findOne({val: "b"}, function (err, result) {
+                    assert(!err, "Error Occurred");
+                    assert.equal(result.stats1.counter1, -1, "Invalid stats value")
+                    assert.equal(result.stats1[year].counter1, -1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month].counter1, -1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month][day].counter1, -1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month][day][hour].counter1, -1, "Invalid stats value")
+                    assert.equal(result.stats1.counter2, 1, "Invalid stats value")
+                    assert.equal(result.stats1[year].counter2, 1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month].counter2, 1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month][day].counter2, 1, "Invalid stats value")
+                    assert.equal(result.stats1[year][month][day][hour].counter2, 1, "Invalid stats value")
                     return done();
                 })
             })
 
         });
-
-
     });
-
-
 });
